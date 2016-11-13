89561c7fd2cf9c4359c7791309ef4dca20dc60c4
aha_android <aha_android@163.com>
2016-11-14 00:33:41 +0800

    origin finiish cjh

--- app/src/main/java/cjh/authnumberview/AuthNumberView.java
+++ app/src/main/java/cjh/authnumberview/AuthNumberView.java
@@ -5,10 +5,18 @@ import android.content.res.TypedArray;
 import android.graphics.Color;
 import android.os.Build;
 import android.support.annotation.RequiresApi;
+import android.text.Editable;
+import android.text.InputFilter;
+import android.text.InputType;
 import android.text.TextUtils;
+import android.text.TextWatcher;
 import android.util.AttributeSet;
 import android.util.DisplayMetrics;
+import android.util.Log;
+import android.util.TypedValue;
 import android.view.Gravity;
+import android.view.KeyEvent;
+import android.view.MotionEvent;
 import android.view.View;
 import android.widget.EditText;
 import android.widget.LinearLayout;
@@ -23,20 +31,20 @@ public class AuthNumberView extends LinearLayout {
 
     private static final int DEFAULT_NUMBER_COUNT = 4;
 
-    private static final int DEFAULT_WH = 30;
+    private static final int DEFAULT_WH = 50;
 
     private static final int DEFAULT_MARGIN = 5;
 
-    private static final int DEFAULT_CODETEXTSIZE = 16;
+    private static final int DEFAULT_CODETEXTSIZE = 22;
 
     private static final int DEFAULT_CODETEXTCOLOR = 0xe36b1f;
 
-    private static final int DEFAULT_CODEPADDING = 3;
+    private static final int DEFAULT_CODEPADDING = 2;
 
     private static final int DEFAULT_BGDRAWABLE = R.drawable.selector_default;
 
     private Context context;
-    
+
     private DisplayMetrics dm;
 
     private int number_count = DEFAULT_NUMBER_COUNT;
@@ -53,6 +61,7 @@ public class AuthNumberView extends LinearLayout {
 
     private int codeBgDrawable = DEFAULT_BGDRAWABLE;
 
+    private EditText selectView;
 
     private ArrayList<EditText> list = new ArrayList<>();
 
@@ -83,15 +92,72 @@ public class AuthNumberView extends LinearLayout {
         dm = getResources().getDisplayMetrics();
         initialAttrs(attrs);
         initEditText();
+        addOnTouchListener();
+        addTextChangeWatcher();
+        setSelectView(list.get(0));
+    }
+
+    private void addTextChangeWatcher() {
+        for (int j = 0; j < list.size(); j++) {
+            final int finalJ = j;
+            list.get(j).addTextChangedListener(new TextWatcher() {
+                @Override
+                public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+
+                }
+
+                @Override
+                public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
+                    if (charSequence.length() >= 1) {
+                        if (finalJ != list.size() - 1)
+                            setSelectView(list.get(finalJ + 1));
+                    }
+//                    else {
+//                        if (finalJ != 0) {
+//                            EditText et = list.get(finalJ - 1);
+//                            setSelectView(et);
+//                        }
+//                    }
+                }
+
+                @Override
+                public void afterTextChanged(Editable editable) {
+
+                }
+            });
+        }
+    }
+
+    private void addOnTouchListener() {
+        for (final EditText et : list) {
+            et.setOnTouchListener(new OnTouchListener() {
+                @Override
+                public boolean onTouch(View view, MotionEvent motionEvent) {
+                    setSelectView(et);
+                    return false;
+                }
+            });
+        }
+    }
+
+    private void setSelectView(EditText editText) {
+        editText.requestFocus();
+        selectView = editText;
     }
 
     private void initEditText() {
         EditText editText = null;
+        margin = dip2px(margin);
+        codePadding = dip2px(codePadding);
         for (int i = 0; i < number_count; i++) {
             editText = new EditText(context);
             editText.setGravity(Gravity.CENTER);
+            editText.setFilters(new InputFilter[]{new InputFilter.LengthFilter(1)});
+            editText.setCursorVisible(false);
+            editText.setInputType(InputType.TYPE_CLASS_NUMBER);
             editText.setTextColor(codeTextColor);
             editText.setPadding(codePadding, codePadding, codePadding, codePadding);
+            editText.setTextSize(TypedValue.COMPLEX_UNIT_SP, codeTextSize);
             int pxWH = dip2px(wh);
             LinearLayout.LayoutParams mLp = new LayoutParams(pxWH, pxWH);
             mLp.setMargins(margin, margin, margin, margin);
@@ -99,7 +165,25 @@ public class AuthNumberView extends LinearLayout {
             editText.setBackgroundResource(codeBgDrawable);
             addView(editText);
             list.add(editText);
+            editText = null;
         }
+    }
+
+    @Override
+    public boolean dispatchKeyEvent(KeyEvent event) {
+        if (event.getAction() == KeyEvent.ACTION_DOWN && event.getKeyCode() == KeyEvent.KEYCODE_DEL) {
+            if (selectView != null) {
+                if (!TextUtils.isEmpty(selectView.getText().toString()))
+                    selectView.setText("");
+                else {
+                    int position = list.indexOf(selectView);
+                    if (position != 0)
+                        position = position - 1;
+                    setSelectView(list.get(position));
+                }
+            }
+        }
+        return super.dispatchKeyEvent(event);
     }
 
     private void initialAttrs(AttributeSet attrs) {
